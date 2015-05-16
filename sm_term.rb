b1cfffa9051c905a2369fafe5aa5ac682f8d1130
panchiga <panpanchiga@gmail.com>
2015-05-17 03:19:04 +0900

    push the 'aoeuhtns', rewrite line

--- sm_term.rb
+++ sm_term.rb
@@ -22,33 +22,93 @@ require 'simple_color'
 
 $color = SimpleColor.new
 
-$info = Array.new()
+class SongInfo
 
-class Musicsm
-	#これをすることでこの名前の要素にアクセス出来る
-	attr_accessor :lines 
-	attr_accessor :boxes
-	attr_accessor :levels
-	attr_accessor :filename	
-	attr_accessor :file
 
-	attr_accessor :lf
-	attr_accessor :df
-	attr_accessor :uf
-	attr_accessor :rf
+	attr_accessor:file
+	attr_accessor:filename
+
+	def initialize 
+		@info = Array.new()
+		@filename = ""
+		@file = ""
+	end
+
+	################file########################
+	def set_file()
+		print "input file name:"
+		#while(filename = gets.chop) do
+		@filename = gets.chop
+		if File.exist?(@filename) == false
+			print @filename + " is not found.\n "
+		else
+			@file = open(@filename,"r")
+			p @file
+		end
+	end
+
+	def closefile()
+		@file.close
+	end	
+	################INFORMATION#################
+	def show_info ()
+		#full #TITLE is 
+		#["#TITLE:smooooch・∀・;\r\n"]
+		@info = @file.grep(/#.*;/m)
+		puts @info[0]
+		puts @info[2]
+		puts @info[16]
+		puts @info.size
+
+		#show level
+		#sample
+		#Begginer:    2:
+		require "open3"
+		out = Array.new()
+
+		level = ["Beginner","Easy","Medium","Hard","Challenge"]
+		level.size.times do |tmp|
+			out, err, stat = Open3.capture3("grep -A 1 #{level[tmp]} sm")
+			out_arr = out.split("\r\n")
+			#print tmp
+
+			out_arr.size.times do |i|
+				out_arr[i].strip!
+				#	print sprintf("%10s", out_arr[i]);
+			end	
+			#puts
+			puts sprintf("%10s %5s",out_arr[0], out_arr[1])
+		end
+	end
+
+end
+
+
+class Musicsm < SongInfo
+
+	attr_accessor:lines
+	attr_accessor:lboxes
+	attr_accessor:levels
 
 	def initialize 
 		@lines = Array.new
 		@boxes = Array.new
 		@levels = Array.new
-		@filename = ""
-		@file = ""
-
+		
 		@lf = false
 		@df = false
 		@uf = false
 		@rf = false
 
+		@rl = "h"
+		@rd = "t"
+		@ru = "n"
+		@rr = "s"
+		@ll = "a"
+		@ld = "o"
+		@lu = "e"
+		@lr = "u"
+		#self.set_arrows()
 	end
 	################MUSIC SCORE#################
 	#first, dance score in file to arrays
@@ -70,8 +130,10 @@ class Musicsm
 	end
 
 	################READ FILE###################
-	def readfile()
+	def setScore()
 		mainflag = false
+
+
 		@file.each do |line|
 			if (mainflag == false) and (line[0] == "/") 
 				mainflag = true
@@ -94,60 +156,12 @@ class Musicsm
 		end
 	end
 
-	################INFORMATION#################
-	def show_info ()
-		#full #TITLE is 
-		#["#TITLE:smooooch・∀・;\r\n"]
-		$info = @file.grep(/#.*;/m)
-		puts $info[0]
-		puts $info[2]
-		puts $info[16]
-		puts $info.size
-
-		#show level
-		#sample
-		#Begginer:    2:
-		require "open3"
-		out = Array.new()
-
-		level = ["Beginner","Easy","Medium","Hard","Challenge"]
-		level.size.times do |tmp|
-			out, err, stat = Open3.capture3("grep -A 1 #{level[tmp]} POSSESSION.sm")
-			out_arr = out.split("\r\n")
-			print tmp
-
-			out_arr.size.times do |i|
-				out_arr[i].strip!
-				#	print sprintf("%10s", out_arr[i]);
-			end	
-			#puts
-			puts sprintf("%10s %5s",out_arr[0], out_arr[1])
-		end
-	end
-
-		
-	################file########################
-	def set_file()
-		print "input file name:"
-		#while(filename = gets.chop) do
-		@filename = gets.chop
-		if File.exist?(@filename) == false
-			print @filename + " is not found.\n "
-			#next
-		else
-			@file = open(@filename,"r")
-			#てすとかな
-			#@file.each do |line|
-			#	puts line
-			#end
-		end
-	end
-
-	def closefile()
-		@file.close
+	def show_level(level)
+		p @levels[level]
 	end
 
 	def play(level)
+		out_file = open("#{@filename}_#{level}_play.sm","w")
 		i = 0
 		compare_0 = Array.new(4,"0")
 		@levels[level].each do |box|
@@ -160,8 +174,9 @@ class Musicsm
 					ch_color(j,box.size)
 					transArrow(line[0],line[1],line[2],line[3])
 				end
-				$color.off
 				j += 1
+				out_file.puts(push_arrow())
+				$color.off
 			end
 			i+=1
 			$color.echos(:white, "#{i}----------------")
@@ -208,7 +223,6 @@ class Musicsm
 		@uf = trance(up, l3, @uf)
 		@rf = trance(right, l4, @rf)
 
-		puts
 
 	end	
 
@@ -237,6 +251,70 @@ class Musicsm
 		return ff
 	end
 
+	def set_arrows ()
+
+		puts "leftHand left"
+		@ll = gets.chop
+		puts "leftHand down"
+		@ld = gets.chop
+		puts "leftHand up"
+		@lu = gets.chop
+		puts "leftHand right"
+		@lr = gets.chop
+		
+		puts "rightHand left"
+		@rl = gets.chop
+		puts "rightHand down"
+		@rd = gets.chop
+		puts "rightHand up"
+		@ru = gets.chop
+		puts "rightHand right"
+		@rr = gets.chop
+	end
+
+	def push_arrow
+		arrow = gets.chop
+		str = file_arrow(arrow)
+		
+		return str
+	end
+
+	def file_arrow (str)
+		stamp = "----"
+		str.size.times do |i|
+			case str[i]
+			when @ll
+				stamp[0] = "L"
+			when @ld
+				stamp[1] = "L"
+			when @lu
+				stamp[2] = "L"
+			when @lr
+				stamp[3] = "L"
+			when @rl
+				stamp[0] = "R"
+			when @rd
+				stamp[1] = "R"
+			when @ru
+				stamp[2] = "R"
+			when @rr
+				stamp[3] = "R"
+			end
+		end
+		out_arrows(stamp)
+		return stamp
+	end
+
+	def out_arrows(str)
+		print "\e[1A"
+		
+		str.length.times do |i|
+			print sprintf("%4s",str[i])
+		end
+		puts "          "
+		STDOUT.flush
+	end
+
 end
 
 
@@ -245,11 +323,10 @@ end
 song = Musicsm.new()
 
 song.set_file()
-song.readfile()
-song.show_info()
+#song.show_info()
 
-
-song.play(gets.to_i)
+song.setScore()
+song.play(3)
 
 
 song.closefile()
